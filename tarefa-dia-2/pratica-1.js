8dd594a7f9e926e150610222e956027a032d1ff5
Eru Burel <erica.scheidt.burel@gmail.com>
2022-08-26 18:23:14 -0300

    tentativa uno

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,13 +1,20 @@
-let nome 
-let email 
+let nome
+let email
 let profissao
-let idade  
+let idade
 let lugar 
-nome = erica-scheidt-burel 
-email = erica.burel.scheidt
-profissao = estudante
-idade = 21
-lugar = suecia 
+nome="erica-scheidt-burel"
+email="erica.burel.scheidt@gmail.com"
+profissao = "estudante"
+idade= "21"
+lugar="suecia" 
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+
+
 
 
 
