347fa5a5d5ef7abce38ff070cb7a043ef0ac12ce
Eru Burel <erica.scheidt.burel@gmail.com>
2022-08-28 20:57:55 -0300

    tentativa-numero-uno

--- tarefa-dia-2/pratica-2.js
+++ tarefa-dia-2/pratica-2.js
@@ -6,21 +6,21 @@
 
 let a = 10
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar no "if"- é verdade
 if (a == 10) {
     console.log("Verdade! O valor ", a, " é igual a 10");
 } else {
     console.log("Falso! O valor ", a, " não é igual a 10");
 }
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar no "else"- é falso
 if (a > 10) {
     console.log("Verdade! O valor ", a, " é maior que 10");
 } else {
     console.log("Falso! O valor ", a, " não é maior 10");
 }
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar no "if"- é verdade
 if (10 <= a) {
     console.log("Verdade! O valor ", a, " é maior ou igual a 10");
 } else {
@@ -29,14 +29,14 @@ if (10 <= a) {
 
 let b = true
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar no "if"- é verdadeiro
 if (b == true) {
     console.log("Verdade! O valor ", b, " é igual a true");
 } else {
     console.log("Falso! O valor ", b, " não é igual a true");
 }
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar no "if"- é verdade
 if (b) {
     console.log("Verdade! O valor ", b, " é considerado como verdade");
 } else {
@@ -45,7 +45,7 @@ if (b) {
 
 let c = []
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar como "if"- é verdade 
 if (c) {
     console.log("Verdade! O valor ", c, " é considerado como verdade");
 } else {
@@ -54,30 +54,32 @@ if (c) {
 
 let d = [1,2,3,4]
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar como "if"- é verdade
 if (d) {
     console.log("Verdade! O valor ", d, " é considerado como verdade");
 } else {
     console.log("Falso! O valor ", d, " não é considerado como verdade");
 }
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar como "if"- é verdade
 if (d[0] == 1) {
     console.log("Verdade! O valor ", d[0], " do elemento do índice 0 da variável 'd' é igual a 1");
 } else {
     console.log("Falso! O valor ", d[0], " do elemento do índice 0 da variável 'd' não é igual a 1");
 }
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):  Vai entrar como "else"- é falso
 if (d[1] == 1) {
     console.log("Verdade! O valor ", d[1], " do elemento do índice 1 da variável 'd' é igual a 1");
 } else {
     console.log("Falso! O valor ", d[1], " do elemento do índice 1 da variável 'd' não é igual a 1");
 }
 
+
 let e = "Nome"
 
-// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): 
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'): Vai entrar como "if"- é verdade
+
 if (typeof(e) == 'string') {
     console.log("Verdade! O tipo de dado da variável 'e' é uma string");
 } else {
